e7ea1b91dc2cd888c517fb5253d326cc8b5c63ea
amit <amit.lauwanshi09@gmail.com>
2025-11-14 16:55:26 +0530

    wsgi file changes name for zingmind.settings

--- zingmind/wsgi.py
+++ zingmind/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zingmind_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zingmind.settings')
 
 application = get_wsgi_application()
